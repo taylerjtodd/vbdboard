7d245a4430a3bcbd1df1398aad95212085c65a85
Tayler Todd <taylerjtodd@gmail.com>
2020-07-28 21:51:19 -0500

    starting on data structures

--- app.js
+++ app.js
@@ -2,6 +2,33 @@ var APPLICATION = (function() {
 
     function init() {
         $('#body').html('Hello World');
+        MODEL.init();
+
+        
+        let players = MODEL.players();
+
+        players.sort((a, b) => a.adp - b.adp);
+
+        baselines = {
+            qb: determineBaseline("qb", players)
+        };
+
+    }
+
+    function determineBaseline(pos, players) {
+
+        let starters = MODEL.config().starters;
+
+        let replacementThreshold = starters.qb + starters.rb + starters.wr + starters.te + starters.flex + starters.dst + starters.k
+        replacementThreshold *=  MODEL.config().numTeams;
+
+        for(player in players) {
+            if(player.adp > replacementThreshold && player.pos === pos) {
+                console.log(player);
+                return player;
+            }
+        }
+
     }
 
     return {
